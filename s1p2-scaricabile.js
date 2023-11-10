3184eb20cfb53d7dc8c1f219034d6d56596d6154
Fabio Carucci <garu2000@live.it>
2023-11-11 00:25:53 +0100

    Fine

--- s1p2-scaricabile.js
+++ s1p2-scaricabile.js
@@ -11,37 +11,49 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* 
+1. Number: sono valori numerici compresi tra (-2^53 - 1) e (+2^53 - 1); si possono inserire valori sia interi,
+    sia decimali (separati dalla virgola).
+2. String: è una sequenza di caratteri racchiusi all'interno di apici, doppie virgolette o apici obliqui (backticks); 
+    ex. "Fabio".
+3. Boolean: sono due valori specifici --> true o false.
+4. Undefined: si ottiene quando ad una variabile dichiarata non si assegna alcun valore.
+5. Null: si ottiene quando ad una variabile dichiarata viene assegnato un valore nullo (null != 0)
+ */
 
 /* ESERCIZIO 2
  Descrivi cos'è un oggetto in JavaScript, con parole tue.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-
-/* ESERCIZIO 3
- Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+/* 
+In javascript, un oggetto è un'insieme di informazioni a cui viene associato un valore che può essere
+    data type (number, string, boolean, undefined o null) o anche structural type (oggetto, array o funzione);
+    per questo motivo si dice che un oggetto ha un formato chiave-valore.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* console.log(12 + 20); // output: 32 */
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let x = 12; */
 
 /* ESERCIZIO 5
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let name = "Fabio" */
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* console.log(4 - x); //output: -8 */
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +62,9 @@ REGOLE
  NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* 
+let name1="john";
+let name2="John";
+console.log(name1===name2); // output: false
+console.log(name1.toLowerCase()===name2.toLowerCase()); //output: true
+*/
